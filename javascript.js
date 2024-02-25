8367da8f91a6ec2b376221404391e866a5d49944
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-02-25 05:29:56 -0500

    Add 16x16 square grid

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+
+for (let i = 1; i <= 16; i++) {
+  let rowDiv = document.createElement('div');
+  rowDiv.setAttribute('class', 'row');
+  rowDiv.setAttribute('id', `row-${i}`);
+  container.appendChild(rowDiv);
+  for (let j = 1; j <= 16; j++) {
+    let squareDiv = document.createElement('div');
+    squareDiv.setAttribute('class', 'square');
+    squareDiv.setAttribute('id', `${i}-${j}`);
+    rowDiv.appendChild(squareDiv);
+  }
+}
