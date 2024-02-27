b6dc5a7c399bd29f7b7864b86f47ac1b5994601d
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-02-27 04:55:20 -0500

    Add logic for sqaures to change colors with each interaction

--- javascript.js
+++ javascript.js
@@ -1,12 +1,10 @@
-const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
-
-function randomIndex () {
-  return Math.floor(Math.random() * 15);
+function randomRGB() {
+  return Math.floor(Math.random() * 255);
 }
 
 const container = document.querySelector('.container');
 
-function removeGrid () {
+function removeGrid() {
   let rows = document.querySelectorAll('.row');
   for (let i = 0; i < rows.length; i++) {
     container.removeChild(rows[i]);
@@ -20,7 +18,6 @@ btn.addEventListener('click', function (e) {
   while (gridSize < 10 || gridSize > 100 || isNaN(gridSize)) {
     gridSize = +prompt('Enter a number between 10 & 100');
   }
-  console.log(gridSize);
   for (let i = 1; i <= gridSize; i++) {
     let rowDiv = document.createElement('div');
     rowDiv.setAttribute('class', 'row');
@@ -31,8 +28,10 @@ btn.addEventListener('click', function (e) {
       squareDiv.setAttribute('class', 'square');
       squareDiv.setAttribute('id', `${i}-${j}`);
       squareDiv.addEventListener('mouseover', function (e) {
-        let randomColor = `#${hexValues[randomIndex()]}${hexValues[randomIndex()]}${hexValues[randomIndex()]}${hexValues[randomIndex()]}${hexValues[randomIndex()]}${hexValues[randomIndex()]}`
-        squareDiv.style.backgroundColor = randomColor;
+        let redVal = randomRGB();
+        let greenVal = randomRGB();
+        let blueVal = randomRGB();
+        squareDiv.style.backgroundColor = `rgb(${redVal}, ${greenVal}, ${blueVal})`;
       });
       rowDiv.appendChild(squareDiv);
     }
